b0bb8030a82c14dcf0c1254a6944412a7f328afe
korri <dragstyles@hotmail.de>
2018-09-14 12:35:12 +0200

    http stuff

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 
-// const app = require('http').createServer((req, res) => res.send('Ahoy!'));
-// const PORT = process.env.PORT || 3000;
+const app = require('http').createServer((req, res) => res.send('Ahoy!'));
+const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server is listening on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
 
 const bot = new Discord.Client({disableEveryone: true});
